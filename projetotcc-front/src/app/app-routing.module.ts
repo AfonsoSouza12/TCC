d0712cf4c73712b5cd83bb6dae8c3451dd4d0356
Afonso <tomzerabr@hotmail.com>
2021-03-29 21:30:07 -0300

    adicionado etapa e subetapa nas routes

--- projetotcc-front/src/app/app-routing.module.ts
+++ projetotcc-front/src/app/app-routing.module.ts
@@ -1,16 +1,19 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
 import {PrincipalComponent} from './principal/principal.component';
 import {CargoComponent} from './cargo/cargo.component';
 
-const routes: Routes =[{
-  path: '', component: PrincipalComponent},
-  {path: 'cargo', component: CargoComponent}
-]
+const routes: Routes = [
+  {path: '', component: PrincipalComponent},
+  {path: 'cargo', component: CargoComponent},
+  {path: 'etapa', component: CargoComponent},
+  {path: 'subEtapa', component: CargoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
