3e189b30acceaa4a5b85fea6209465c5c2cfd880
Afonso <tomzerabr@hotmail.com>
2021-05-07 12:40:47 -0300

    ajustes login

--- projetotcc-back/src/main/java/br/edu/utfpr/pb/projetotcc/config/ResourceServerConfig.java
+++ projetotcc-back/src/main/java/br/edu/utfpr/pb/projetotcc/config/ResourceServerConfig.java
@@ -14,7 +14,8 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-				.antMatchers("/**").permitAll()
+				.antMatchers("/etapa").hasRole("ADMIN")
+				.antMatchers("/**").authenticated()
 				.anyRequest().authenticated();
 	}
 	
