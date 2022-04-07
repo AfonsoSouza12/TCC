e216cca2f8145ddc1bca59c1dfe3e0d66408cd6e
Afonso <tomzerabr@hotmail.com>
2022-04-06 21:49:39 -0300

    Quadro Scrum | separado um quadro para cada status com filter na lista de findall

--- projetotcc-front/src/app/quadro/quadro.component.ts
+++ projetotcc-front/src/app/quadro/quadro.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {Solicitacao} from '../model/solicitacao';
 import {SolicitacaoService} from '../solicitacao/solicitacao.service';
-import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+import {StatusOpt} from '../../shared/consts/StatusOpt';
 
 @Component({
   selector: 'app-quadro',
@@ -10,17 +11,43 @@ import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
 })
 export class QuadroComponent implements OnInit {
 
-  solicitacoes: Solicitacao[];
+  statuss = Object.keys(StatusOpt).map(key => ({ label: StatusOpt[key], value: key }));
+  solicitacoes: Solicitacao[] = [];
+
+  solicitacoesBacklog: Solicitacao[];
+  solicitacoesTodo: Solicitacao[];
+  solicitacoesDoing: Solicitacao[];
+  solicitacoesTest: Solicitacao[];
+  solicitacoesDone: Solicitacao[];
   constructor(private solicitacaoService: SolicitacaoService) { }
 
   ngOnInit() {
     this.findSolicitacoes();
+
   }
 
   findSolicitacoes() {
-    this.solicitacaoService.findAll().subscribe( e => this.solicitacoes = e);
+    this.solicitacaoService.findAll().subscribe( e => {
+      this.solicitacoes = e;
+      this.solicitacoesBacklog = this.solicitacoes.filter(solicitacao => solicitacao.status === "Backlog");
+      this.solicitacoesTodo = this.solicitacoes.filter(solicitacao => solicitacao.status === "To Do");
+      this.solicitacoesDoing = this.solicitacoes.filter(solicitacao => solicitacao.status === "Doing");
+      this.solicitacoesTest = this.solicitacoes.filter(solicitacao => solicitacao.status === "Test");
+      this.solicitacoesDone = this.solicitacoes.filter(solicitacao => solicitacao.status === "Done");
+    }
+    );
+
   }
-  drop(event: CdkDragDrop<string[]>) {
-    moveItemInArray(this.solicitacoes, event.previousIndex, event.currentIndex);
+  drop(event: CdkDragDrop<Object[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
   }
 }
--- projetotcc-front/src/app/solicitacao/solicitacao.service.ts
+++ projetotcc-front/src/app/solicitacao/solicitacao.service.ts
@@ -4,6 +4,8 @@ import {Etapa} from '../model/etapa';
 import {HttpClient} from '@angular/common/http';
 import {environment} from '../../environments/environment';
 import {Solicitacao} from '../model/solicitacao';
+import {Observable} from 'rxjs';
+import {SubEtapa} from '../model/sub-etapa';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +14,9 @@ export class SolicitacaoService extends CrudService<Solicitacao, number>{
 
   constructor(http: HttpClient) {
     super(environment.api + '/solicitacao', http);
+  }
+
+  findByStatus(status: string): Observable<Solicitacao[]>{
+    return this.http.get<Solicitacao[]>(`${this.getUrl()}/solicitacao/busca-status/${status}`);
   }
 }
